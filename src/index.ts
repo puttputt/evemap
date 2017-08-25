db9962eed4624cc464d6ffc810036168085f61d6
Kyle Smyth <Kyle.Smyth@iqmetrix.com>
2017-08-25 16:49:45 -0600

    Express!

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import "reflect-metadata";
+import {Request, Response} from "express";
 import {createConnection} from "typeorm";
 import {getEntityManager} from "typeorm";
 import {SolarSystem} from "./models/SolarSystem";
 import {Region} from "./models/Region";
+import * as express from "express";
+import * as bodyParser from "body-parser";
 
-// connection settings are in the "ormconfig.json" file
+const app = express();
+
 createConnection().then(async connection => {
     console.log("Connection established");
-
-    let regionRepo = getEntityManager().getRepository(Region);
-    const regions = await regionRepo.find({ relations: ["systems"]});
-
-    console.log(regions);
-
-    console.log(regions[0].systems);
-
 }).catch(error => console.log("Error: ", error));
+
+app.listen(3000);
+
+console.log("Express application running on port 3000");
